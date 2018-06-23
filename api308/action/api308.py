664f2214a810c348ac34c7306ffc472c1120c2a0
bushitan <373514952@qq.com>
2018-06-23 08:56:18 +0800

    2.2.1

--- api308/action/api308.py
+++ api308/action/api308.py
@@ -4,39 +4,23 @@ import os,sys
 
 appid = 'VxfXp4cu'
 appsecret = 'dc5c7986daef50c1e02ab09b442ee34f'
+
 class ActionAPI308():
 	def _post(self,url,data):
-		# req = urllib2.Request(url)
-		# opener = urllib2.build_opener(urllib2.HTTPCookieProcessor())
-		# response = opener.open(req)
-		# _json =  json.loads(response.read())
-		# return  _json
-# post请求
-# import urllib
-# import urllib2
-#
 		data_json = json.dumps(data)
-		# test_data_urlencode = urllib.urlencode(data)
 		headerdata = {"Content-type": "application/json"}
 		req = urllib2.Request( url, data_json ,headerdata)
 		# print req
 		res_data = urllib2.urlopen(req)
-		print res_data
+		# print res_data
 		res = res_data.read()
-		print res
 		return res
-# print res
 
 	def _get(self,url):
 		req = urllib2.Request(url)
 		response = urllib2.urlopen(req)
 		_json =  json.loads(response.read())
 		return  _json
-
-
-		# opener = urllib2.build_opener(urllib2.HTTPCookieProcessor())
-		# response = opener.open(req)
-		# _json =  json.loads(response.read())
 
 	def _token_file(self):
 		ABSPATH=os.path.abspath(sys.argv[0])
@@ -52,94 +36,173 @@ class ActionAPI308():
 		access_token = load_dict["data"]["access_token"]
 		return access_token
 
-	#创建token
+	## 1. 获取API访问令牌
 	def create(self):
 		_url = "https://api.308308.com/auth/token?app_id=%s&app_secret=%s" %(appid,appsecret)
-		_json = self._post(_url)
+		_json = self._get(_url)
 		print _json
 		with open(self._token_file(),'w') as f:
 			json.dump(_json,f)
 		return  self._get_token()
 
-	# 刷新token
-	def refresh_token(self):
-		_url = "https://api.308308.com/auth/refresh_token?token=%s" %(self._get_token())
-		_json = self._post(_url)
-		print _json
-		return
-
-	# 校验token
-	def check_token(self):
-		_url = "https://api.308308.com/auth/check_token?token=%s" %(self._get_token())
-		_json = self._post(_url)
-		print _json
-		return
-
-
-	def _json_check(self):
-		return
-
-
-	# 获取行业
-	def cms_get_all_industry(self):
-		# return u'111'
-
-		_url = "https://api.308308.com/cms/articles/get_all_industry?access_token=%s" %(self._get_token())
-		_json = self._get(_url)
-		print _json
-		return _json
-
-	# 获取行业下的目录
-	def cms_get_categories_by_industry(self):
-		# return u'112'
-		_url = "https://api.308308.com/cms/articles/get_categories_by_industry?access_token=%s" %(self._get_token())
+	## 4 通过openid获取用户信息
+	def user_get_info_by_openid(self,open_id):
+		_url = "https://api.308308.com/cms/ca/get_info_by_openid?access_token=%s" %(self._get_token())
 		_data = {
-		  "industry_id":"1"
+		  "open_id":open_id
 		}
 		return self._post(_url,_data)
 
-	# 获取目录下的封面列表
-	def cms_get_articles_by_category(self):
-		return u'112'
-		_url = "https://api.308308.com/cms/articles/get_articles_by_category?access_token=%s" %(self._get_token())
-		return self._get(_url)
 
-	# 获取文章
-	def cms_get_article (self):
-		return u'112'
+	## 5 用户绑定
+	def user_bind(self,open_id,account,password):
+		_url = "https://api.308308.com/cms/ca/bind?access_token=%s" %(self._get_token())
+		_data = {
+			"openid": open_id,
+			"account": account,
+			"password": password
+		}
+		return self._post(_url,_data)
+
+	## 6.用户注册
+	def user_register(self,open_id,mobile,code,industry_id):
+		_url = "https://api.308308.com/cms/ca/register?access_token=%s" %(self._get_token())
+		_data = {
+			"openid": open_id,
+			"mobile": mobile,
+			"code" : code,
+			"industry_id":industry_id
+		}
+		return self._post(_url,_data)
+
+	## 7.套餐购买
+	def user_buy_package(self,uid,industry_id,member_type,validation_from,validation_to):
+		_url = "https://api.308308.com/cms/ca/buy_package?access_token=%s" %(self._get_token())
+		_data = {
+			"uid": uid,
+			"industry_id":industry_id,
+			"member_type":member_type,
+			"validation_from" : validation_from,
+			"validation_to" : validation_to
+		}
+		return self._post(_url,_data)
+
+	# 8 获取行业
+	def cms_get_all_industry(self):
+		# return u'111'
+		_url = "https://api.308308.com/cms/articles/get_all_industry?access_token=%s" %(self._get_token())
+		_json = self._get(_url)
+		return _json
+
+	# 9 获取行业下的目录
+	def cms_get_categories_by_industry(self,industry_id):
+		# return u'112'
+		_url = "https://api.308308.com/cms/articles/get_categories_by_industry?access_token=%s" %(self._get_token())
+		_data = {
+		  "industry_id":industry_id
+		}
+		return self._post(_url,_data)
+
+	# 10 获取指定行业对应栏目下的文章列表
+	def cms_get_articles_by_category(self,
+									 industry_id,category_id,
+									 rows,page_no,
+									 sort,order):
+		_url = "https://api.308308.com/cms/articles/get_articles_by_category?access_token=%s" %(self._get_token())
+		_data = {
+			"industry_id": industry_id,
+			"category_id": category_id,
+			"rows": rows,
+			"page_no":page_no,
+			"sort": sort,
+			"order": order
+		}
+		return self._post(_url,_data)
+
+	# 11 获取指定的文章
+	def cms_get_article (self,uid,article_id):
 		_url = "https://api.308308.com/cms/articles/get_article?access_token=%s" %(self._get_token())
-		return self._get(_url)
+		_data = {
+			  "uid" : uid,
+			  "article_id": article_id
+			}
+		return self._post(_url,_data)
 
 
 	############# 权限 #################
-	def ca_has_privilege (self):
-		return u'112'
+	## 12 判断有无权限
+	def ca_has_privilege (self,uid,industry_id,bussiness_type,bussiness_id):
 		_url = "https://api.308308.com/cms/ca/has_privilege?access_token=%s" %(self._get_token())
-		return self._get(_url)
+		_data ={
+			"uid" : uid,
+			"industry_id":industry_id,
+			"bussiness_type": bussiness_type,
+			"bussiness_id" : bussiness_id
+		}
+		return self._post(_url,_data)
 
 
 	#########商圈###############
 
-	# 获取行业子类标签列表
-	def cms_get_product_type (self):
-		return u'112'
+	## 13 获取行业子类标签列表
+	def cms_get_product_type (self,industry_id):
 		_url = "https://api.308308.com/cms/get_product_type?access_token=%s" %(self._get_token())
-		return self._get(_url)
+		_data = {
+		   "industry_id":industry_id
+		}
+		return self._post(_url,_data)
 
-	#获取商圈数据
-	def roster_get_list (self):
-		return u'112'
+
+	## 17. 获取商圈数据
+	def roster_get_list (self,
+						 industry_id,category_id,
+						 rows,page_no,
+						 sort,order,
+						 company_name,contact_name,uid):
+
 		_url = "https://api.308308.com/cms/mall/get_malls?access_token=%s" %(self._get_token())
-		return self._get(_url)
+		_data = {
+			"industry_id": industry_id,
+			"rows": rows,
+			"page_no":page_no,
+			"sort": sort,
+			"order": order,
+			"company_name": company_name,
+			"contact_name": contact_name,
+			"uid":uid
+		}
+		_data = {
+		  "sort": "",
+		  "order": "",
+		  "rows": "10",
+		  "page_no": "1",
+		  "industry_id": "1",
+		  "company_name": "广西华思特国际贸易有限公司",
+		  "contact_name": "张三",
+			"uid": "1232"
+		}
+		print _url
+		print _data
+		return self._post(_url,_data)
 
 
 
 if __name__ == "__main__":
 	a = ActionAPI308()
 	# print a.create()
+	# print a.user_get_info_by_openid( "oNUgxv608YVIclrLMz_0egqocXcI")
+	# print a.user_bind( "oNUgxv608YVIclrLMz_0egqocXcI","zhangsan","12345678")
+	# print a.user_register( "oNUgxv608YVIclrLMz_0egqocXcI","13800000000","1234",1)
+	print a.user_buy_package( "10092",1,1,"2018-05-23 12:09:12","2019-05-23 12:09:12")
 	# print a.cms_get_all_industry()
-	print a.cms_get_categories_by_industry()
-	pass
+	# print a.cms_get_categories_by_industry(2)
+	# print a.cms_get_articles_by_category(1,3,20,1,"release_date","desc" )
+	# print a.cms_get_article(10921,220634)
+	# print a.ca_has_privilege(10921,1,"BUSSINESS_TYPE_ARTICLE",123)
+	# print a.cms_get_product_type(1)
+	# print a.roster_get_list(1,3,20,1,"release_date","desc" ,u'广西华思',u"张三",1232)
+	# print a.cms_get_categories_by_industry()
+	# pass
 	# print WxHttp('')
 
 
